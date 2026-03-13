54b80af4c843f988b0f2b77c17c03f6877f94110
jaime0717-cpu <jaimelim0717@gmail.com>
2026-03-14 08:22:45 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    printf("%s %s %s\n", "name", "id", "value");
-    printf("%s %010d %05.2f\n", "kim", 12, 4.12);
-    printf("%s %010d %05.2f\n", "lee", 1922, 3.21);
-    printf("%s %010d %05.2f\n", "park", 432, 1.0);
+    printf("%010s %010s %010s\n", "name", "id", "value");
+    printf("%010s %010d % 10.2f\n", "kim", 12, 4.12);
+    printf("%010s %010d % 10.2f\n", "lee", 1922, 3.21);
+    printf("%010s %010d % 10.2f\n", "park", 432, 1.0);
 
+    //%0n은 0으로 채워넣고, 공백은 공백으로 채워넣음
 
     return 0;
 }
