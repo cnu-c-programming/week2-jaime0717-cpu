a2eaad47763d470ee825c530df89986521adc111
jaime0717-cpu <jaimelim0717@gmail.com>
2026-03-14 08:05:19 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -4,8 +4,8 @@ int main()
 {
     int var1 = 100;
     printf("int : %d\n", var1);
-    printf("hex : %o\n", var1);
-    printf("oct : %x\n", var1);
+    printf("hex : %x\n", var1);
+    printf("oct : %o\n", var1);
 
     char var2 = 'c';
     printf("char : %c\n", var2);
