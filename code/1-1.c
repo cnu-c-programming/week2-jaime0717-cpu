d044827a98dc7868caf5e3c556dc45187645ab96
jaime0717-cpu <jaimelim0717@gmail.com>
2026-03-13 12:01:38 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -2,6 +2,21 @@
 
 int main()
 {
+    int var1 = 100;
+    printf("int : %d\n", var1);
+    printf("hex : %o\n", var1);
+    printf("oct : %x\n", var1);
+
+    char var2 = 'c';
+    printf("char : %c\n", var2);
+
+    char* var3 = "hello world!";
+    printf("string : %s\n", var3);
+
+    float var 4 = 3.141592;
+    printf("float : %f\n", var4);
+    printf("exp : %e\n", var4);
+    
 
     return 0;
 }
