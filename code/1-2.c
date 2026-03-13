ead22000b53666013e635b43cd10fc1f7815f2ba
jaime0717-cpu <jaimelim0717@gmail.com>
2026-03-13 12:20:23 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 int main()
 {
+    printf("%s %s %s\n", "name", "id", "value");
+    printf("%s %010d %05.2f\n", "kim", 12, 4.12);
+    printf("%s %010d %05.2f\n", "lee", 1922, 3.21);
+    printf("%s %010d %05.2f\n", "park", 432, 1.0);
 
 
     return 0;
