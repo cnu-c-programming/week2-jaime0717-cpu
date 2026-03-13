a59ea6650aab6c193b80b181a561293f0a3f9b67
jaime0717-cpu <jaimelim0717@gmail.com>
2026-03-14 08:42:44 +0900

    Update 2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -2,14 +2,14 @@
 
 int main()
 {
+
     printf("char: %u\n", sizeof(char));
     printf("short: %u\n", sizeof(short));
     printf("int: %u\n", sizeof(int));
-    printf("unsighned int: %u\n", sizeof(unsigned int));
+    printf("unsigned int: %u\n", sizeof(unsigned int));
     printf("long: %u\n", sizeof(long));
     printf("long long: %u\n", sizeof(long long));
     printf("float: %u\n", sizeof(float));
-    
 
     return 0;
 }
