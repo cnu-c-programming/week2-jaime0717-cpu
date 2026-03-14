6c8c8e85cedaf4803632fbb1841c505c6590b578
jaime0717-cpu <jaimelim0717@gmail.com>
2026-03-14 10:08:44 +0900

    Update 2-3.c

--- code/2-3.c
+++ code/2-3.c
@@ -7,10 +7,9 @@ int main()
     unsigned char* addr = (unsigned char*)&x;
 
     printf("%x\n", *addr);
-    printf("%x\n", *(addr+1);
-    printf("%x\n", *(addr+2);
-    printf("%x\n", *(addr+3);
+    printf("%x\n", *(addr+1));
+    printf("%x\n", *(addr+2));
+    printf("%x\n", *(addr+3));
 
     return 0;
 }
-
